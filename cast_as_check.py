68fbaad83c35645a003f63af6f42418b9b470681
Michael Vitousek <mmvitousek@gmail.com>
2013-03-26 01:51:23 -0700

    forgot to include the factored-out cast functions

---
+++ cast_as_check.py
@@ -0,0 +1,18 @@
+from typing import has_type
+import inspect
+
+# Casts 
+# Cast-as-check
+def retic_cast(val, src, trg, msg):
+    # Can't (easily) just call retic_cas_check because of frame introspection resulting in
+    # incorrect line number reporting
+    assert has_type(val, trg), "%s at line %d (expected %s)" % (msg, inspect.currentframe().f_back.f_lineno, trg)
+    return val
+
+def retic_check(val, trg, msg):
+    assert has_type(val, trg), "%s at line %d" % (msg, inspect.currentframe().f_back.f_lineno)
+    return val
+
+def retic_error(msg):
+    assert False, "%s at line %d" % (msg, inspect.currentframe().f_back.f_lineno)
+    
