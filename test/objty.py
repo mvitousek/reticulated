96ae09da8e6928959bc30aac87d6e3ae408e9723
Michael Vitousek <mmvitousek@gmail.com>
2014-02-05 13:48:43 -0500

    asdde

---
+++ test/objty.py
@@ -0,0 +1,15 @@
+class B:
+    foo = 42
+    def g(self, c:C):
+        pass
+
+class C:
+    x = 1
+    def f(self, x:int, y:C, z:Self, w:B)->int:
+        return x + self.x
+
+def f(x:C):
+    pass
+
+def g(x:C.Class):
+    pass
---
+++ test/objty2.py
@@ -0,0 +1,11 @@
+
+
+class X:
+    def f(self, x:X, y:Y, z:Z):
+        pass
+class Y:
+    def f(self, x:X, y:Y, z:Z):
+        pass
+class Z:
+    def f(self, x:X, y:Y, z:Z):
+        pass
