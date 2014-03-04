8e802c117d145af333362215fcafa5eaa83ad4f2
Michael Vitousek <mmvitousek@gmail.com>
2014-03-04 17:01:00 -0500

    Added geddit test case

---
+++ case_studies/genshi/geddit/controller.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import operator, os, pickle, sys
+import cherrypy
+
+class Root(object):
+
+    def __init__(self, data):
+        self.data = data
+
+    @cherrypy.expose
+    def index(self):
+        return 'Geddit'
+
+
+def main(filename):
+    data = {} # We'll replace this later
+
+    # Some global configuration; note that this could be moved into a
+    # configuration file
+    cherrypy.config.update({
+        'tools.encode.on': True, 'tools.encode.encoding': 'utf-8',
+        'tools.decode.on': True,
+        'tools.trailing_slash.on': True,
+        'tools.staticdir.root': os.path.abspath(os.path.dirname(__file__)),
+    })
+
+    cherrypy.quickstart(Root(data), '/', {
+        '/media': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': 'static'
+        }
+    })
+
+if __name__ == '__main__':
+    main(sys.argv[1])
