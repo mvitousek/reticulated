ff61e764c62efbef11fa6f44c602aa5aec15ff4b
Steev Young <steevyoung@yahoo.com>
2014-10-01 15:52:53 -0400

    updating regression testing

--- case_studies/steevs/OutputString.py
+++
@@ -1,8 +0,0 @@
-def main():
-    get_name()
-    print('Hello', name)
-
-def get_name()-> String:
-    name = input('Enter your name: ')
-
-main()
--- test/Files.py
+++
@@ -1,8 +0,0 @@
-33.py
-BodyMassIndex.py
-CtoO.py
-Def.py
-Error?.py
-Interest.py
-MagicDates.py
-MaximumValue.py
