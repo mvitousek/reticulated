9342f18c508b7bde5b0ef6f3602b223776efde7d
migeed-z <migeed.z@outlook.com>
2016-09-13 11:07:01 -0400

    fix tests and add comments

--- unit_tests/test_proposition.py
+++ unit_tests/test_proposition.py
@@ -6,6 +6,18 @@ from sympy import *
 sys.path.insert(0, '..')
 from retic.proposition import *
 
+# Creating our own generator
+# instead of using uuid.uuid3()
+# in order to be able to test the
+# generated symbols
+#
+# However all generated symbols in this
+# test suite are unique. So we do not
+# test different instances of a
+# proposition class being equal using
+# that transformer.
+
+
 def myGen(n):
     yield n
     yield n + 1
@@ -15,7 +27,7 @@ class TestTransformReduce(unittest.TestCase):
     p2 = Prim_P('y', str)
     p3 = Prim_P('x', int)
 
-    x1, x2, x3, x4 = symbols('0 1 2 3')
+    x1, x2, x3 = symbols('0 1 2')
 
     def test_and_1(self):
 
@@ -28,16 +40,11 @@ class TestTransformReduce(unittest.TestCase):
         g = count(start=0, step=1)
         p_or1 = OrProp([OrProp([self.p1, self.p2]), self.p2])
         res = p_or1.transform_and_reduce(next, g)
-        self.assert_(res[0].equals(Or(self.x1, self.x2, self.x3)))
+        self.assert_(res[0].equals(Or(self.x2, self.x3, self.x1)))
 
 
     def test_all(self):
-        g = count(start=0, step=1)
         p_all = NotProp(AndProp([self.p3, self.p1]))
-        print(p_all.transform_and_reduce(next, g))
-
-    def test_all2(self):
-        p_all = NotProp(AndProp([OrProp([self.p1, self.p2]), self.p3]))
         res = p_all.transform_and_reduce(None)
-        print(res)
+        assert len(res[1]) == 1
 
